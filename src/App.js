352cbbe896b5b2c309ce5222f37891d2b9b399e1
ARISE01 <ghoshsoumik378@gmail.com>
2023-11-12 22:26:07 +0530

    Created Navbar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/Navbar.js';
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils" subhead1="About Us" subhead2="Contact Us"/>
+    </>
+  );
+}
+
+export default App;
